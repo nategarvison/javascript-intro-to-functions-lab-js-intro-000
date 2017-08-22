03b67b7fd526d6a55e8b186d61d1a0725c3b7cf6
nategarvison <ngarvison@gmail.com>
2017-08-22 02:24:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ function sayHiToGrandma(string) {
   if (string === 'hello') {
     return 'I can\'t hear you!'
   }
-  if (string === 'HELLO') {
+  else if (string === 'HELLO') {
     return 'YES INDEED!'
+  }
+  else if 'I love you, Grandma.' {
+    return 'I love you, too.'
   }
 }
